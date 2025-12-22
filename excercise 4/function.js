e7277f8bcd9349adc3da2fccc0bde66d39419026
EngAmiira <abdiyaasmin6@gmail.com>
2025-12-22 16:57:56 +0400

    submited execrscie 4 and execrcise 5

---
+++ excercise 4/function.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 3));   
+console.log(add(10, 20)); 
+console.log(add(-2, 7));  
+
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(5, 3));   
+console.log(add(10, 20)); 
+console.log(add(-2, 7));  g
---
+++ excercise 5/arrow.js
@@ -0,0 +1,7 @@
+
+const add = (a, b) => a + b;
+
+
+console.log(add(5, 3));   
+console.log(add(10, 20)); 
+console.log(add(-2, 7));  
